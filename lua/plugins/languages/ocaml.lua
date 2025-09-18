2052fc39e5fdded7b2096f9fd4406b83f073db1f
thoxy <thi.hur67@gmail.com>
2025-09-18 18:32:55 +0200

    fix ocaml

--- lua/plugins/languages/ocaml.lua
+++ lua/plugins/languages/ocaml.lua
@@ -3,39 +3,6 @@
 -- lua/plugins/languages/ocaml.lua
 -- ============================================================================
 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-
--- Configure OCaml Language Server
-vim.lsp.config.ocamllsp = {
-  on_attach = on_attach,
-  filetypes = {
-    "ml",
-    "mli",
-    "cmi",
-    "cmo",
-    "cmx",
-    "cma",
-    "cmxa",
-    "cmxs",
-    "cmt",
-    "cmti",
-    "opam",
-  },
-  root_markers = { "*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace", "*.ml" },
-  settings = {
-    ocamllsp = {
-      filetypes = {
-        "ocaml",
-        "ocaml.menhir", -- Parser generator
-        "ocaml.interface", -- Interface files
-        "ocaml.ocamllex", -- Lexer generator
-        "reason", -- Reason syntax
-        "dune", -- Dune build files
-      },
-    },
-  },
-}
-
 return {
   -- Treesitter support for OCaml
   {
@@ -45,5 +12,46 @@ return {
         vim.list_extend(opts.ensure_installed, { "ocaml" })
       end
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ocamllsp = {
+          filetypes = {
+            "ocaml",
+            "ocaml.menhir",
+            "ocaml.interface",
+            "ocaml.ocamllex",
+            "reason",
+            "dune",
+            "ml",
+            "mli",
+            "cmi",
+            "cmo",
+            "cmx",
+            "cma",
+            "cmxa",
+            "cmxs",
+            "cmt",
+            "cmti",
+            "opam",
+          },
+          root_markers = {
+            function(name)
+              return name:match ".*%.opam$"
+            end,
+            "esy.json",
+            "package.json",
+            ".git",
+            "dune-project",
+            "dune-workspace",
+            function(name)
+              return name:match ".*%.ml$"
+            end,
+          },
+        },
+      },
+    },
   },
 }
