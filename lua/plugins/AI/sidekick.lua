7ad618418ba1971a2df35cdc383de03d990ca3f2
thoxy <thi.hur67@gmail.com>
2025-09-29 11:42:30 +0200

    add lualine copilot infos from sidekick

--- lua/plugins/AI/sidekick.lua
+++ lua/plugins/AI/sidekick.lua
@@ -1,5 +1,40 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        copilot = {},
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local icons = {
+        Error = { " ", "DiagnosticError" },
+        Inactive = { " ", "MsgArea" },
+        Warning = { " ", "DiagnosticWarn" },
+        Normal = { " ", "Special" },
+      }
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local status = require("sidekick.status").get()
+          return status and vim.tbl_get(icons, status.kind, 1)
+        end,
+        cond = function()
+          return require("sidekick.status").get() ~= nil
+        end,
+        color = function()
+          local status = require("sidekick.status").get()
+          local hl = status and (status.busy and "DiagnosticWarn" or vim.tbl_get(icons, status.kind, 2))
+          return { fg = require("Snacks").util.color(hl) }
+        end,
+      })
+    end,
+  },
+  {
     "folke/sidekick.nvim",
     opts = {
       -- add any options here
