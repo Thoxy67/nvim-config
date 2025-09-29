15e9aad31f790d04b85bcd005527a55c00c9733d
thoxy <thi.hur67@gmail.com>
2025-09-29 12:08:24 +0200

    add keybinding for fyler.nvim

--- lua/plugins/general/productivity/fyler.lua
+++ lua/plugins/general/productivity/fyler.lua
@@ -2,6 +2,15 @@ return {
   {
     "A7Lavinraj/fyler.nvim",
     cmd = { "Fyler" },
+    keys = {
+      {
+        "<leader>f1",
+        function()
+          require("fyler").open()
+        end,
+        desc = "Open Fyler file manager",
+      },
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = { icon_provider = "nvim_web_devicons" },
     config = function(_, opts)
