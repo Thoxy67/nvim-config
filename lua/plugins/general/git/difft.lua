ade55dac1d95f19bd9df8d2024a190c119fb2bb7
thoxy <thi.hur67@gmail.com>
2025-10-21 07:46:11 +0200

    Add difft.nvim for jj diff

---
+++ lua/plugins/general/git/difft.lua
@@ -0,0 +1,25 @@
+return {
+  --luacheck: globals Difft
+  {
+    "ahkohd/difft.nvim",
+    keys = {
+      {
+        "<leader>dD",
+        function()
+          if Difft.is_visible() then
+            Difft.hide()
+          else
+            Difft.diff()
+          end
+        end,
+        desc = "Toggle Difft",
+      },
+    },
+    config = function()
+      require("difft").setup {
+        command = "jj diff --no-pager", -- or "GIT_EXTERNAL_DIFF='difft --color=always' git diff"
+        layout = "float", -- nil (buffer), "float", or "ivy_taller"
+      }
+    end,
+  },
+}
